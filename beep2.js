70e97de8a4a2f6146ac25286c21dd4640a74a5b6
Leonid Buneev <buneev.leonid@gmail.com>
2021-04-24 13:54:51 +0200

    Update beep2.js

--- beep2.js
+++ beep2.js
@@ -20,6 +20,6 @@ play(sequence) {
   if(!el) return
   if(!Array.isArray(el)) el = [el]
   window.Beeper.beep(el[0] || 520, el[1] || 200, el[2] || 100)
-  setTimeout(() => window.Beeper.play(sequence), el[1] || 200)
+  setTimeout(() => window.Beeper.play(sequence), el[1] || 200 + 50)
 }
 }
